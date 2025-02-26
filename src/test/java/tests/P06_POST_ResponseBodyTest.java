0d2cad4ad1ec40099881e1d1fb4497e1871039b7
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-26 19:05:42 +0300

    New

--- src/test/java/tests/P06_POST_ResponseBodyTest.java
+++ src/test/java/tests/P06_POST_ResponseBodyTest.java
@@ -1,7 +1,10 @@
 package tests;
-
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class P06_POST_ResponseBodyTest {
 
@@ -23,17 +26,26 @@ public class P06_POST_ResponseBodyTest {
     */
 
     @Test
-    public void postResponseBodyTest(){
+    public void postResponseBodyTest() {
         // 1- EndPoint ve reqBody hazırlanır.
         String url = "https://jsonplaceholder.typicode.com/posts";
 
         JSONObject reqBody = new JSONObject();
-        reqBody.put("title","API");
-        reqBody.put("body","API ogrenmek ne guzel");
-        reqBody.put("userId",10);
+        reqBody.put("title", "API");
+        reqBody.put("body", "API ogrenmek ne guzel");
+        reqBody.put("userId", 10);
 
         // 2- Expected Data verilmemiş.
 
+        // 3- Response kaydedilir.
+        Response response = given().contentType(ContentType.JSON).when().body(reqBody.toString()).post(url);
 
+        // 4- Assertion işlemleri yapılır.
+        response.then().assertThat()
+                .statusCode(201)
+                .contentType("application/json")
+                .body("title", equalTo("API"),
+                        "userId", lessThan(100),
+                        "body", containsString("API"));
     }
 }
