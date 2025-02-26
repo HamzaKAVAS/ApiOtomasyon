37c2c48c7e0c4a2c6f90c128f42815529834c8d9
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-26 23:51:53 +0300

    New

--- src/test/java/tests/P07_GET_BodyTekrarlardanKurtulma.java
+++ src/test/java/tests/P07_GET_BodyTekrarlardanKurtulma.java
@@ -1,14 +1,13 @@
 package tests;
-
 import io.restassured.response.Response;
 import org.junit.Test;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class P07_GET_BodyTekrarlardanKurtulma {
 
     /*
-       https://restful-booker.herokuapp.com/booking/100 url'ine
+       https://restful-booker.herokuapp.com/booking/99 url'ine
        bir GET request gonderdigimizde donen Response'un,
        status code'unun 200,
        ve content type'inin application/json; charset=utf-8,
@@ -21,9 +20,9 @@ public class P07_GET_BodyTekrarlardanKurtulma {
     */
 
     @Test
-    public void test(){
+    public void test() {
         // 1- EndPoint hazırlanır.
-        String url = "https://restful-booker.herokuapp.com/booking/100";
+        String url = "https://restful-booker.herokuapp.com/booking/99";
 
         // 2- Expected Data verilmemiş.
 
@@ -31,7 +30,13 @@ public class P07_GET_BodyTekrarlardanKurtulma {
         Response response = given().when().get(url);
 
         // 4- Assertion işlemleri yapılır.
-
-
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .body("firstname", equalTo("John"),
+                        "lastname", equalTo("Smith"),
+                        "totalprice", equalTo(111),
+                        "depositpaid", equalTo(true),
+                        "additionalneeds", equalTo("Breakfast"));
     }
 }
