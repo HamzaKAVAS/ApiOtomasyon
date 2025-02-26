ddccd7115f1247d8b3f1c411075c7d745cb479c1
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-26 23:26:07 +0300

    New

---
+++ src/test/java/tests/P07_GET_BodyTekrarlardanKurtulma.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.junit.Test;
+
+public class P07_GET_BodyTekrarlardanKurtulma {
+
+    /*
+       https://restful-booker.herokuapp.com/booking/100 url'ine
+       bir GET request gonderdigimizde donen Response'un,
+       status code'unun 200,
+       ve content type'inin application/json; charset=utf-8,
+       ve response body'sindeki
+       "firstname"in, "John",
+       ve "lastname"in, "Smith",
+       ve "totalprice"in, 111,
+       ve "depositpaid"in, true,
+       ve "additionalneeds"in, "Breakfast" oldugunu test edin
+    */
+
+    @Test
+    public void test(){
+
+    }
+}
