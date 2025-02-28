71f24205e90d1d9a0df16b85da45fbf5e4d98ebe
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-28 22:44:36 +0300

    New

---
+++ src/test/java/tests/P09_GET_ExpectedDataOlusturma.java
@@ -0,0 +1,20 @@
+package tests;
+
+public class P09_GET_ExpectedDataOlusturma {
+
+    /*
+
+        https://jsonplaceholder.typicode.com/posts/22 url'ine
+        bir GET request yolladigimizda donen response body'sinin
+        asagida verilen ile ayni oldugunu test ediniz
+
+        Response body :(EXPECTED DATA)
+    {
+    "userId": 3,
+    "id": 22,
+    "title": "dolor sint quo a velit explicabo quia nam",
+    "body": "eos qui et ipsum ipsam suscipit aut sed omnis non odio expedita earum mollitia molestiae aut atque rem suscipit nam impedit esse"
+    }
+
+    */
+}
