2bcf982b2f68df88114ce09cdfdbfe288c39ec71
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-28 22:51:20 +0300

    New

--- src/test/java/tests/P09_GET_ExpectedDataOlusturma.java
+++ src/test/java/tests/P09_GET_ExpectedDataOlusturma.java
@@ -1,7 +1,10 @@
 package tests;
 
+import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class P09_GET_ExpectedDataOlusturma {
 
@@ -23,15 +26,20 @@ public class P09_GET_ExpectedDataOlusturma {
 
     @Test
     public void test(){
-        // 1- Endpoint hazırlama
+        // 1- Endpoint hazırlanır.
         String url = "https://jsonplaceholder.typicode.com/posts/22";
 
-        // 2- Expected Data
+        // 2- Expected Data hazırlanır.
         JSONObject expBody = new JSONObject();
         expBody.put("userId",3);
         expBody.put("id",22);
         expBody.put("title","dolor sint quo a velit explicabo quia nam");
         expBody.put("body","eos qui et ipsum ipsam suscipit aut sed omnis non odio expedita earum mollitia molestiae aut atque rem suscipit nam impedit esse");
+
+        // 3- Response kaydedilir.
+        Response response = given().when().get(url);
+
+        // 4- Assertion işlemleri
 
     }
 }
