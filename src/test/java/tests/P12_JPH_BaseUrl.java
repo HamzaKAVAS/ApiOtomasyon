25d6b36e49068787c158018c8cabc56adbee6c37
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-04 22:49:35 +0300

    04.03.2025

--- src/test/java/tests/P12_JPH_BaseUrl.java
+++ src/test/java/tests/P12_JPH_BaseUrl.java
@@ -1,7 +1,10 @@
 package tests;
 
 import baseUrl.JPH_BaseUrl;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class P12_JPH_BaseUrl extends JPH_BaseUrl {
 
@@ -16,5 +19,10 @@ public class P12_JPH_BaseUrl extends JPH_BaseUrl {
     public void test01(){
         // 1- Endpoint Hazırlanır.
         specJPH.pathParam("pp1","posts");
+
+        // 2- Expected Body Yok.
+
+        // 3- Response Kaydedilir.
+        Response response = given().spec(specJPH).when().get("/{pp1}");
     }
 }
