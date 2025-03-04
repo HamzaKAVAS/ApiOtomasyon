fc234e134e4ac726f8fd9dfb87056582bc21a2f6
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-04 22:46:02 +0300

    04.03.2025

--- src/test/java/tests/P12_JPH_BaseUrl.java
+++ src/test/java/tests/P12_JPH_BaseUrl.java
@@ -1,7 +1,20 @@
 package tests;
 
 import baseUrl.JPH_BaseUrl;
+import org.testng.annotations.Test;
 
 public class P12_JPH_BaseUrl extends JPH_BaseUrl {
 
+    /*
+        https://jsonplaceholder.typicode.com/posts endpointine
+        bir GET request gonderdigimizde
+        donen response'un status code'unun 200 oldugunu ve
+        Response'ta 100 kayit oldugunu test edin
+    */
+
+    @Test
+    public void test01(){
+        // 1- Endpoint Hazırlanır.
+        specJPH.pathParam("pp1","posts");
+    }
 }
