8030e51be9a199dacf40b61dec24e08efde671d9
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-04 22:53:35 +0300

    04.03.2025

--- src/test/java/tests/P12_JPH_BaseUrl.java
+++ src/test/java/tests/P12_JPH_BaseUrl.java
@@ -1,10 +1,9 @@
 package tests;
-
 import baseUrl.JPH_BaseUrl;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class P12_JPH_BaseUrl extends JPH_BaseUrl {
 
@@ -16,13 +15,23 @@ public class P12_JPH_BaseUrl extends JPH_BaseUrl {
     */
 
     @Test
-    public void test01(){
+    public void test01() {
         // 1- Endpoint Hazırlanır.
-        specJPH.pathParam("pp1","posts");
+        specJPH.pathParam("pp1", "posts");
 
         // 2- Expected Body Yok.
 
         // 3- Response Kaydedilir.
         Response response = given().spec(specJPH).when().get("/{pp1}");
+
+        // 4- Assertion İşlemleri Yapılır.
+        response.then().assertThat().statusCode(200).body("booking", hasSize(100));
     }
+
+    /*
+       https://jsonplaceholder.typicode.com/posts/44 endpointine
+       bir GET request gonderdigimizde donen response'un
+       status code'unun 200 oldugunu ve "title" degerinin
+       "optio dolor molestias sit" oldugunu test edin
+    */
 }
