a0b16e3ee574a79e8a08c5db6a11276731bf88aa
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-05 18:26:24 +0300

    05032025

--- src/test/java/tests/P15_HerOkuAppQueryParams.java
+++ src/test/java/tests/P15_HerOkuAppQueryParams.java
@@ -29,4 +29,26 @@ public class P15_HerOkuAppQueryParams extends RESTFULL_BaseUrl {
         // 4- Assertion İşlemleri Yapılır.
         response.then().assertThat().statusCode(200).body("size()", greaterThan(0));
     }
+
+    /*
+       https://restful-booker.herokuapp.com/booking endpointine
+       gerekli Query parametrelerini yazarak
+       “firstname” degeri “Eric” olan rezervasyon oldugunu test edecek
+       bir GET request gonderdigimizde, donen response’un status code’unun
+       200 oldugunu ve “Eric” ismine sahip en az bir booking var olup olmadıgını test edin
+    */
+
+    @Test
+    public void test02() {
+        // 1- EndPoint ve Query Hazırlanır.
+        specRestFull.pathParam("pp1", "booking").queryParams("firstname", "Eric");
+
+        // 2- Expected Body Yok.
+
+        // 3- Response Kaydedilir.
+        Response response = given().spec(specRestFull).when().get("/{pp1}");
+
+        // 4- Assertion İşlemleri Yapılır.
+        response.then().assertThat().statusCode(200).body("size()", greaterThanOrEqualTo(0));
+    }
 }
