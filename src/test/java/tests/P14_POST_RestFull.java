4b9f0a4be93ae1d8c7bf90ad3bdb346e60b315e9
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-05 17:46:33 +0300

    05032025

--- src/test/java/tests/P14_POST_RestFull.java
+++ src/test/java/tests/P14_POST_RestFull.java
@@ -1,8 +1,11 @@
 package tests;
-
 import baseUrl.RESTFULL_BaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class P14_POST_RestFull extends RESTFULL_BaseUrl {
 
@@ -24,22 +27,35 @@ public class P14_POST_RestFull extends RESTFULL_BaseUrl {
     */
 
     @Test
-    public void postRestFullTest(){
+    public void postRestFullTest() {
         // 1- EndPoint ve reqBody Hazırlanır.
-        specRestFull.pathParam("pp1","booking");
+        specRestFull.pathParam("pp1", "booking");
 
         JSONObject bookingdates = new JSONObject();
-        bookingdates.put("checkin","2025-07-01");
-        bookingdates.put("checkout","2025-07-15");
+        bookingdates.put("checkin", "2025-07-01");
+        bookingdates.put("checkout", "2025-07-15");
 
         JSONObject reqBody = new JSONObject();
-        reqBody.put("firstname","Kerem");
-        reqBody.put("lastname","Bürsin");
-        reqBody.put("totalprice",1275);
-        reqBody.put("depositpaid",true);
-        reqBody.put("bookingdates",bookingdates);
-        reqBody.put("additionalneeds","Breakfast,Dinner,Wi-fi");
+        reqBody.put("firstname", "Kerem");
+        reqBody.put("lastname", "Bürsin");
+        reqBody.put("totalprice", 1275);
+        reqBody.put("depositpaid", true);
+        reqBody.put("bookingdates", bookingdates);
+        reqBody.put("additionalneeds", "Breakfast,Dinner,Wi-fi");
 
         // 2- Expected Body Yok.
+
+        // 3- Response Kaydedilir.
+        Response response = given().contentType(ContentType.JSON).spec(specRestFull).when().body(reqBody.toString()).post("/{pp1}");
+
+        // 4- Assertion İşlemleri Yapılır.
+        response.then().assertThat().statusCode(200).
+                body("booking.firstname", equalTo("Kerem"),
+                        "booking.lastname", equalTo("Bürsin"),
+                        "booking.totalprice", equalTo(1275),
+                        "booking.depositpaid", equalTo(true),
+                        "booking.bookingdates.checkin", equalTo("2025-07-01"),
+                        "booking.bookingdates.checkout", equalTo("2025-07-15"),
+                        "booking.additionalneeds", equalTo("Breakfast,Dinner,Wi-fi"));
     }
 }
