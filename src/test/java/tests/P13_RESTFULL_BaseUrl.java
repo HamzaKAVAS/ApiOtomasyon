87f52991ef5a03e1f41094001c73e07854a2ab9f
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-05 17:24:59 +0300

    05032025

--- src/test/java/tests/P13_RESTFULL_BaseUrl.java
+++ src/test/java/tests/P13_RESTFULL_BaseUrl.java
@@ -1,6 +1,11 @@
 package tests;
+import baseUrl.RESTFULL_BaseUrl;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
 
-public class P13_RESTFULL_BaseUrl {
+public class P13_RESTFULL_BaseUrl extends RESTFULL_BaseUrl {
 
     /*
        https://restful-booker.herokuapp.com/booking endpointine
@@ -9,5 +14,17 @@ public class P13_RESTFULL_BaseUrl {
        Response'ta 12 bookingid'sine sahip booking oldugunu test edin
     */
 
+    @Test
+    public void restFullBaseTest() {
+        // 1- EndPoint Hazırlanır.
+        specRestFull.pathParam("pp1", "booking");
 
+        // 2- Expected Body Yok.
+
+        // 3- Response Kaydedilir.
+        Response response = given().spec(specRestFull).when().get("/{pp1}");
+
+        // 4- Assertion İşlemleri Yapılır.
+        response.then().assertThat().statusCode(200).body("bookingid", hasItem(12));
+    }
 }
