c1ffcde63cea127030f0e6f3ec03480ed72dd9a9
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-05 17:15:21 +0300

    05032025

---
+++ src/test/java/tests/P13_RESTFULL_BaseUrl.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class P13_RESTFULL_BaseUrl {
+
+    /*
+       https://restful-booker.herokuapp.com/booking endpointine
+       bir GET request gonderdigimizde donen response'un
+       status code'unun 200 oldugunu ve
+       Response'ta 12 bookingid'sine sahip booking oldugunu test edin
+    */
+
+
+}
