0f970d72dc6e357172794b2a9fe565aa67a597b6
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-06 18:14:14 +0300

    06032025

--- src/test/java/testDatas/JPHDatas.java
+++ src/test/java/testDatas/JPHDatas.java
@@ -1,4 +1,18 @@
 package testDatas;
 
+import org.json.JSONObject;
+
 public class JPHDatas {
+
+    public static int basariliStatusCode = 200;
+
+    public static JSONObject expData(){
+        JSONObject expBody = new JSONObject();
+        expBody.put("userId",3);
+        expBody.put("id",22);
+        expBody.put("title","dolor sint quo a velit explicabo quia nam");
+        expBody.put("body","eos qui et ipsum ipsam suscipit aut sed omnis non odio expedita earum mollitia molestiae aut atque rem suscipit nam impedit esse");
+
+        return expBody;
+    }
 }
--- src/test/java/tests/P16_GET_TestDataClassKullanimi.java
+++ src/test/java/tests/P16_GET_TestDataClassKullanimi.java
@@ -1,5 +1,29 @@
 package tests;
 
-public class P16_GET_TestDataClassKullanimi {
+import baseUrl.JPH_BaseUrl;
+import org.testng.annotations.Test;
 
+public class P16_GET_TestDataClassKullanimi extends JPH_BaseUrl {
+
+    /*
+       https://jsonplaceholder.typicode.com/posts/22 url'ine bir GET request yolladigimizda donen response'in
+       status kodunun 200 ve
+       response body'sinin asagida verilen ile ayni oldugunu test ediniz
+       Response body : (expected data)
+       {
+         "userId": 3,
+         "id": 22,
+         "title": "dolor sint quo a velit explicabo quia nam",
+         "body": "eos qui et ipsum ipsam suscipit aut sed omnis non odio expedita earum mollitia molestiae aut atque rem suscipit nam impedit esse"
+       }
+    */
+
+    @Test
+    public void testDataKullanimi1(){
+        // 1- EndPoint ve PathParams Hazırlanır.
+        specJPH.pathParams("pp1","posts","pp2",22);
+
+        // 2- Expected Body Yazılır.
+
+    }
 }
