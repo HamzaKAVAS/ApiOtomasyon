41f5d2cd87c619b911e2e947b8bf65cbd4903789
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-07 18:27:39 +0300

    07032025

--- src/test/java/tests/P17_PUT_TestDataKullanimi.java
+++ src/test/java/tests/P17_PUT_TestDataKullanimi.java
@@ -1,18 +1,18 @@
 package tests;
-
 import baseUrl.JPH_BaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 import testDatas.JPHDatas;
-
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.*;
 
 public class P17_PUT_TestDataKullanimi extends JPH_BaseUrl {
 
     /*
-       https://jsonplaceholder.typicode.com/posts/70 url'ine asagidaki body'e sahip
+       https://jsonplaceholder.typicode.com/posts/7 url'ine asagidaki body'e sahip
        bir PUT request yolladigimizda donen response'in
        status kodunun 200, content type'inin "application/json; charset=utf-8",
        Connection header degerinin "keep-alive"
@@ -34,9 +34,9 @@ public class P17_PUT_TestDataKullanimi extends JPH_BaseUrl {
     */
 
     @Test
-    public void test01(){
+    public void test01() {
         // 1- EndPoint ve reqBody Hazırlanır.
-        specJPH.pathParams("pp1","posts","pp2",70);
+        specJPH.pathParams("pp1", "posts", "pp2", 7);
         JSONObject reqBody = JPHDatas.jsonData();
 
         // 2- Expected Body Yazılır.
@@ -45,6 +45,16 @@ public class P17_PUT_TestDataKullanimi extends JPH_BaseUrl {
         // 3- Response Kaydedilir.
         Response response = given().contentType(ContentType.JSON).spec(specJPH).when().body(reqBody.toString()).put("/{pp1}/{pp2}");
 
+        // 4- Assertion İşlemleri Yapılır.
+        JsonPath resJP = response.jsonPath();
 
+        assertEquals(response.getStatusCode(), JPHDatas.basariliStatusCode);
+        assertEquals(response.getContentType(), JPHDatas.ContentType);
+        assertEquals(response.getHeader("Connection"), JPHDatas.ConnectionHeader);
+
+        assertEquals(resJP.get("title"), expBody.get("title"));
+        assertEquals(resJP.get("body"), expBody.get("body"));
+        assertEquals(resJP.get("userId"), expBody.get("userId"));
+        assertEquals(resJP.get("id"), expBody.get("id"));
     }
 }
