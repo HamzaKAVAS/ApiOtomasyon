b3df153cc5c6f5564a02e9f9df08fe896a4d0f21
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-15 22:10:15 +0300

    150325

---
+++ src/test/java/tests/P18_POST_TestDataKullanimi.java
@@ -0,0 +1,45 @@
+package tests;
+
+import baseUrl.RESTFULL_BaseUrl;
+
+public class P18_POST_TestDataKullanimi extends RESTFULL_BaseUrl {
+
+    /*
+       https://restful-booker.herokuapp.com/booking url'ine asagidaki body'ye sahip
+       bir POST request gonderdigimizde donen response'un
+       id haric asagidaki gibi oldugunu test edin.
+
+       Request body
+         {
+           "firstname" : "Ahmet",
+           "lastname" : "Bulut",
+           "totalprice" : 500,
+           "depositpaid" : false,
+           "bookingdates" :
+              {
+                "checkin" : "2021-06-01",
+                "checkout" : "2021-06-10"
+              },
+           "additionalneeds" : "wi-fi"
+         }
+       Response Body
+         {
+           "bookingid": 24,
+           "booking":
+              {
+                "firstname": "Ahmet",
+                "lastname": "Bulut",
+                "totalprice": 500,
+                "depositpaid": false,
+                "bookingdates":
+                  {
+                    "checkin": "2021-06-01",
+                    "checkout": "2021-06-10"
+                  },
+                "additionalneeds": "wi-fi"
+              }
+         }
+    */
+
+
+}
