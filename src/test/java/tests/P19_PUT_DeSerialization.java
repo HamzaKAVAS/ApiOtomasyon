60c904af674e7add8cf3fa2f07d83082c1375bdd
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-18 17:39:45 +0300

    180325

--- src/test/java/tests/P19_PUT_DeSerialization.java
+++ src/test/java/tests/P19_PUT_DeSerialization.java
@@ -63,5 +63,8 @@ public class P19_PUT_DeSerialization extends JPH_BaseUrl {
 
         // 2- Expected Body
         Map<String, Object> expMapBody = JPHDatas.mapDataOlusturParametreli("Ahmet","Merhaba",10.0,70.0);
+
+        // 3- Response Kaydedilir
+        Response response = given().contentType(ContentType.JSON).spec(specJPH).when().body(reqMapBody).put("/{pp1}/{pp2}");
     }
 }
