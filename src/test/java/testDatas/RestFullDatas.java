6f48e0d28afd26eb7d72bd8486dfe6bbb559803a
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-19 18:26:38 +0300

    190325

--- src/test/java/testDatas/RestFullDatas.java
+++ src/test/java/testDatas/RestFullDatas.java
@@ -1,7 +1,6 @@
 package testDatas;
 
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
---
+++ src/test/java/tests/P20_POST_DeSerialization.java
@@ -0,0 +1,54 @@
+package tests;
+
+import baseUrl.RESTFULL_BaseUrl;
+import org.testng.annotations.Test;
+import testDatas.RestFullDatas;
+
+import java.util.Map;
+
+public class P20_POST_DeSerialization extends RESTFULL_BaseUrl {
+
+    /*
+       https://restful-booker.herokuapp.com/booking url'ine
+       asagidaki body'ye sahip bir POST request gonderdigimizde
+       donen response'un id haric asagidaki gibi oldugunu test edin.
+
+         Request body
+           {
+              "firstname" : "Ahmet",
+              "lastname" : "Bulut",
+              "totalprice" : 500,
+              "depositpaid" : false,
+              "bookingdates" :
+              {
+                 "checkin" : "2021-06-01",
+                 "checkout" : "2021-06-10"
+              },
+              "additionalneeds" : "wi-fi"
+           }
+
+         Response Body // expected data
+           {
+              "bookingid": 24,
+              "booking":
+              {
+              "firstname": "Ahmet",
+              "lastname": "Bulut",
+              "totalprice": 500,
+              "depositpaid": false,
+              "bookingdates":
+              {
+                 "checkin": "2021-06-01",
+                 "checkout": "2021-06-10"
+              },
+              "additionalneeds": "wi-fi"
+           }
+     */
+
+    @Test
+    public void test01(){
+        // 1- Endpoint ve reqBody Hazırlanır
+        specRestFull.pathParams("pp1","booking");
+        Map<String,Object> reqBody = RestFullDatas.mapDataOLustur();
+    }
+}
