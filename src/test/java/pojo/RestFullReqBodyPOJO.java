65fc55181c11dce69621d79246593d21cea75d97
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-21 17:48:11 +0300

    210325

---
+++ src/test/java/pojo/RestFullReqBodyPOJO.java
@@ -0,0 +1,20 @@
+package pojo;
+
+public class RestFullReqBodyPOJO {
+
+    /*
+       Request body
+           {
+              "firstname" : "Ahmet",
+              "lastname" : "Bulut",
+              "totalprice" : 500,
+              "depositpaid" : false,
+              "bookingdates" :
+              {
+                 "checkin" : "2021-06-01",
+                 "checkout" : "2021-06-10"
+              },
+              "additionalneeds" : "wi-fi"
+           }
+    */
+}
