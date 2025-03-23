1d07c0ddfd8002d23914054f6937eb7cf6ffe1ee
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-23 21:52:00 +0300

    230325

--- src/test/java/pojo/RestFullReqBodyPOJO.java
+++ src/test/java/pojo/RestFullReqBodyPOJO.java
@@ -25,4 +25,53 @@ public class RestFullReqBodyPOJO {
     private boolean depositpaid;
     private RestFullBookingdatesPOJO bookingdates;
     private String additionalneeds;
+
+    // 2- Tüm variable'lar için getter and setter olusturuyoruz.
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(int totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public boolean isDepositpaid() {
+        return depositpaid;
+    }
+
+    public void setDepositpaid(boolean depositpaid) {
+        this.depositpaid = depositpaid;
+    }
+
+    public RestFullBookingdatesPOJO getBookingdates() {
+        return bookingdates;
+    }
+
+    public void setBookingdates(RestFullBookingdatesPOJO bookingdates) {
+        this.bookingdates = bookingdates;
+    }
+
+    public String getAdditionalneeds() {
+        return additionalneeds;
+    }
+
+    public void setAdditionalneeds(String additionalneeds) {
+        this.additionalneeds = additionalneeds;
+    }
 }
