e98829fcc52a1e6c7294d69ae753d946318c56ad
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-24 16:28:22 +0300

    240325

--- src/test/java/pojo/RestFullExpBodyPOJO.java
+++ src/test/java/pojo/RestFullExpBodyPOJO.java
@@ -26,4 +26,21 @@ public class RestFullExpBodyPOJO {
     private int bookingid;
     private RestFullReqBodyPOJO booking;
 
+    // 2- Tüm variable'lar için getter and setter olusturuyoruz.
+    public int getBookingid() {
+        return bookingid;
+    }
+
+    public void setBookingid(int bookingid) {
+        this.bookingid = bookingid;
+    }
+
+    public RestFullReqBodyPOJO getBooking() {
+        return booking;
+    }
+
+    public void setBooking(RestFullReqBodyPOJO booking) {
+        this.booking = booking;
+    }
+
 }
