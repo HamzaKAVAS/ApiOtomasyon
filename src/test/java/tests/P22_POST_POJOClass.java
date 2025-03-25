b41b5d1f8817c334e990d6b8b84d44f518c984ca
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-25 15:20:21 +0300

    250325

--- src/test/java/tests/P22_POST_POJOClass.java
+++ src/test/java/tests/P22_POST_POJOClass.java
@@ -1,6 +1,9 @@
 package tests;
 
 import baseUrl.RESTFULL_BaseUrl;
+import org.testng.annotations.Test;
+import pojo.RestFullBookingdatesPOJO;
+import pojo.RestFullReqBodyPOJO;
 
 public class P22_POST_POJOClass extends RESTFULL_BaseUrl {
 
@@ -39,4 +42,13 @@ public class P22_POST_POJOClass extends RESTFULL_BaseUrl {
               }
            }
     */
+
+    @Test
+    public void test01(){
+        // 1- Endpoint ve reqBody Hazırlanması
+        specRestFull.pathParam("pp1", "booking");
+        RestFullBookingdatesPOJO bookingdates = new RestFullBookingdatesPOJO("2021-06-01", "2021-06-10");
+        RestFullReqBodyPOJO reqBody = new RestFullReqBodyPOJO("Hayri", "Yiğit", 275, true, bookingdates, "wi-fi");
+
+    }
 }
