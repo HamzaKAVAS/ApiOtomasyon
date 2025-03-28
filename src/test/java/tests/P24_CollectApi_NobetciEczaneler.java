ef019a8b1771964765dbdaa7a34fdf1b2ddeec3d
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-28 12:36:01 +0300

    280325

---
+++ src/test/java/tests/P24_CollectApi_NobetciEczaneler.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class P24_CollectApi_NobetciEczaneler {
+
+    /*
+       https://api.collectapi.com/health/dutyPharmacy?il=Istanbul&ilce=Üsküdar
+       adresine asağıdaki bilgilerle bir GET request yapınca
+       dönen cevabı yazdırınız.
+
+       header
+       Content-Type : application/json
+       Authorization : apikey 7oz12GLMsEHKccL5kM3kah:5t9yvaRbrmIzEjsWljC330
+    */
+}
