e6a7d4b31a42e9b6bb5f0b52542aa5a26b277c29
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-28 13:00:07 +0300

    280325

--- src/test/java/tests/P24_CollectApi_NobetciEczaneler.java
+++ src/test/java/tests/P24_CollectApi_NobetciEczaneler.java
@@ -1,7 +1,10 @@
 package tests;
 
 import baseUrl.CollectApiBaseUrl;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class P24_CollectApi_NobetciEczaneler extends CollectApiBaseUrl {
 
@@ -19,6 +22,14 @@ public class P24_CollectApi_NobetciEczaneler extends CollectApiBaseUrl {
     public void test01(){
         // 1- Endpoint Hazırlanması
         specCollectApi.pathParams("pp1", "health", "pp2", "dutyPharmacy")
-                .queryParams("il", "Istanbul", "ilce", "Üsküdar");
+                .queryParams("il", "Bursa", "ilce", "Osmangazi");
+
+        // 2- Expected Body Yok
+
+        // 3- Response Kaydedilir
+        Response response = given().spec(specCollectApi).when().header("Content-Type", "application/json")
+                .header("Authorization", "apikey 7oz12GLMsEHKccL5kM3kah:5t9yvaRbrmIzEjsWljC330").get("/{pp1}/{pp2}");
+
+        response.prettyPrint();
     }
 }
