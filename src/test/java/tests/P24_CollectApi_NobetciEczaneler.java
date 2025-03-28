4df27ac029ac6e7fb0ace82c3ef4fe3f5146ca88
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-28 12:57:11 +0300

    280325

--- src/test/java/tests/P24_CollectApi_NobetciEczaneler.java
+++ src/test/java/tests/P24_CollectApi_NobetciEczaneler.java
@@ -1,6 +1,9 @@
 package tests;
 
-public class P24_CollectApi_NobetciEczaneler {
+import baseUrl.CollectApiBaseUrl;
+import org.testng.annotations.Test;
+
+public class P24_CollectApi_NobetciEczaneler extends CollectApiBaseUrl {
 
     /*
        https://api.collectapi.com/health/dutyPharmacy?il=Istanbul&ilce=Üsküdar
@@ -11,4 +14,11 @@ public class P24_CollectApi_NobetciEczaneler {
        Content-Type : application/json
        Authorization : apikey 7oz12GLMsEHKccL5kM3kah:5t9yvaRbrmIzEjsWljC330
     */
+
+    @Test
+    public void test01(){
+        // 1- Endpoint Hazırlanması
+        specCollectApi.pathParams("pp1", "health", "pp2", "dutyPharmacy")
+                .queryParams("il", "Istanbul", "ilce", "Üsküdar");
+    }
 }
