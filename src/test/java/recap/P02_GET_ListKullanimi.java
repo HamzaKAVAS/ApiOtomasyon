b26115ad95fd4fe52d4cf9f2409cebcc57edf4ea
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-29 14:38:37 +0300

    290325

---
+++ src/test/java/recap/P02_GET_ListKullanimi.java
@@ -0,0 +1,13 @@
+package recap;
+
+import baseUrl.ReqResBaseUrl;
+
+public class P02_GET_ListKullanimi extends ReqResBaseUrl {
+
+    /*
+       https://reqres.in/api/users?page=2 URL'sine bir GET isteği gönderin.
+       Gelen yanıtın durum kodunun 200 olduğunu doğrulayın.
+       Liste içerisindeki kullanıcı sayısının en az 1 olduğunu doğrulayın.
+       İlk kullanıcının email adresinin boş olmadığını kontrol edin.
+    */
+}
