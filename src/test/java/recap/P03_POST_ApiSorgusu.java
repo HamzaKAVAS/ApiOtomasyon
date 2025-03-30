2087f6931cdb28145aa7b39189be5d5fdb1b120e
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-30 09:24:58 +0300

    300325

---
+++ src/test/java/recap/P03_POST_ApiSorgusu.java
@@ -0,0 +1,17 @@
+package recap;
+
+import baseUrl.ReqResBaseUrl;
+
+public class P03_POST_ApiSorgusu extends ReqResBaseUrl {
+
+    /*
+       https://reqres.in/api/users URL'sine
+       header derğeri "Content-Type", "application/json" olan bir POST isteği gönderin.
+       İstek için JSON formatında şu bilgileri içeren bir body kullanın:
+         { "name": "morpheus",
+           "job": "leader" }
+       Gelen yanıtın durum kodunun 201 olduğunu doğrulayın.
+       Yanıtın JSON body'sinde name alanının morpheus olduğunu kontrol edin.
+    */
+
+}
