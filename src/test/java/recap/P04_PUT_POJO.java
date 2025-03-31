4ba23a6905717c839ca9c73169711c0f60ca6257
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-31 13:51:05 +0300

    310325

---
+++ src/test/java/recap/P04_PUT_POJO.java
@@ -0,0 +1,16 @@
+package recap;
+
+import baseUrl.ReqResBaseUrl;
+
+public class P04_PUT_POJO extends ReqResBaseUrl {
+
+    /*
+       https://reqres.in/api/users/2 URL'sine header değeri "Content-Type","application/json" olan bir PUT isteği gönderin.
+       Kullanıcıyı şu şekilde güncelleyin:
+         {
+           "name": "John Doe",
+           "job": "Manager"
+         }
+       Yanıtın durum kodunun 200 olduğunu doğrulayın ve name alanının John Doe olduğunu doğrulayın.
+    */
+}
