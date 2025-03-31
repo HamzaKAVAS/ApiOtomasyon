784ebf22936d478407b9d4d13b27327da5e5d095
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-31 14:02:27 +0300

    310325

--- src/test/java/recap/P04_PUT_POJO.java
+++ src/test/java/recap/P04_PUT_POJO.java
@@ -1,6 +1,8 @@
 package recap;
 
 import baseUrl.ReqResBaseUrl;
+import org.junit.jupiter.api.Test;
+import pojo.ReqResPOJO;
 
 public class P04_PUT_POJO extends ReqResBaseUrl {
 
@@ -13,4 +15,13 @@ public class P04_PUT_POJO extends ReqResBaseUrl {
          }
        Yanıtın durum kodunun 200 olduğunu doğrulayın ve name alanının John Doe olduğunu doğrulayın.
     */
+
+    @Test
+    public void putPojoTest(){
+        // 1- EndPoint ve ReqBody Hazırlanır.
+        specReqRes.pathParams("pp1","api","pp2","users","pp3",2);
+        ReqResPOJO reqBody = new ReqResPOJO("John Doe","QA Tester");
+
+        // 2- ExpBody Yok.
+    }
 }
