4d63f81f20f6e30b1dae5f0c9684ca4a5d1dfe40
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-01 11:05:54 +0300

    010425

--- src/test/java/recap/P05_PUT_ResponseBodyTest.java
+++ src/test/java/recap/P05_PUT_ResponseBodyTest.java
@@ -5,8 +5,8 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class P05_PUT_ResponseBodyTest extends ReqResBaseUrl {
 
@@ -29,22 +29,28 @@ public class P05_PUT_ResponseBodyTest extends ReqResBaseUrl {
     */
 
     @Test
-    public void test(){
+    public void test() {
         // 1- EndPoint ve ReqBody Hazırlanır.
-        specReqRes.pathParams("pp1","api","pp2","users","pp3",2);
+        specReqRes.pathParams("pp1", "api", "pp2", "users", "pp3", 2);
         JSONObject reqBody = new JSONObject();
-        reqBody.put("name","Hayri");
-        reqBody.put("job","QA Tester");
+        reqBody.put("name", "Hayri");
+        reqBody.put("job", "QA Tester");
 
         // 2- Expected Body Hazırlanır.
         JSONObject expBody = new JSONObject();
-        expBody.put("name","Hayri");
-        expBody.put("job","QA Tester");
-        expBody.put("updatedAt","2025-03-05T12:34:56.789Z");
+        expBody.put("name", "Hayri");
+        expBody.put("job", "QA Tester");
+        expBody.put("updatedAt", "2025-03-05T12:34:56.789Z");
 
         // 3- Response Kaydedilir.
         Response response = given().spec(specReqRes).contentType(ContentType.JSON).when().body(reqBody.toString())
                 .put("/{pp1}/{pp2}/{pp3}");
         //response.prettyPrint();
+
+        // 4- Assertions İşlemleri Yapılır.
+        assertEquals(200, response.getStatusCode());
+        assertEquals("application/json; charset=utf-8", response.getContentType());
+        assertEquals(expBody.get("name"), response.jsonPath().get("name"));
+        assertEquals(expBody.get("job"), response.jsonPath().get("job"));
     }
 }
