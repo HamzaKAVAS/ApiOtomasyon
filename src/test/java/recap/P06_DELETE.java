952c1d1c86626d5fde447787fe3e433b979aea72
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-02 15:08:05 +0300

    020425

---
+++ src/test/java/recap/P06_DELETE.java
@@ -0,0 +1,11 @@
+package recap;
+
+import baseUrl.ReqResBaseUrl;
+
+public class P06_DELETE extends ReqResBaseUrl {
+
+    /*
+       https://reqres.in/api/users/57 URL'sine bir DELETE isteği gönderin.
+       Gelen yanıtın durum kodunun 204 olduğunu doğrulayın.
+    */
+}
