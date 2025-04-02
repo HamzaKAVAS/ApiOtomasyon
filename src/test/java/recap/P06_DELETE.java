d5149a914e1e58b9a4668b003b47affd814c95f5
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-02 15:10:19 +0300

    020425

--- src/test/java/recap/P06_DELETE.java
+++ src/test/java/recap/P06_DELETE.java
@@ -1,6 +1,7 @@
 package recap;
 
 import baseUrl.ReqResBaseUrl;
+import org.junit.jupiter.api.Test;
 
 public class P06_DELETE extends ReqResBaseUrl {
 
@@ -8,4 +9,10 @@ public class P06_DELETE extends ReqResBaseUrl {
        https://reqres.in/api/users/57 URL'sine bir DELETE isteği gönderin.
        Gelen yanıtın durum kodunun 204 olduğunu doğrulayın.
     */
+
+    @Test
+    public void deleteTest(){
+        // 1- EndPoint Hazırlanır.
+        specReqRes.pathParams("pp1","api","pp2","users","pp3",57);
+    }
 }
