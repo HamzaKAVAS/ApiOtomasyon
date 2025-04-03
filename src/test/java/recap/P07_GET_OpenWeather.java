74cf56472092465e23ffa2ce18af39ffc5dee87f
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-03 11:58:39 +0300

    030425

--- src/test/java/recap/P07_GET_OpenWeather.java
+++ src/test/java/recap/P07_GET_OpenWeather.java
@@ -1,5 +1,7 @@
 package recap;
 
+import org.junit.jupiter.api.Test;
+
 public class P07_GET_OpenWeather {
 
     /*
@@ -12,4 +14,11 @@ public class P07_GET_OpenWeather {
        Response Body’sinde "name" alanının "Istanbul" olduğunu,
        Response Body’sinde "main.temp" değerinin -50 ile 50 arasında olduğunu test ediniz.
     */
+
+    static String myApiKey = "89057ad0892940ef7b7f932cab562fbd";
+
+    @Test
+    public void openWeatherTest(){
+
+    }
 }
