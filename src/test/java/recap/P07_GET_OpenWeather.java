9e1d73e6506a740a95fb87615d4d18eb29a3ae2b
HamzaKAVAS <hmzkavas@gmail.com>
2025-04-03 11:56:44 +0300

    030425

---
+++ src/test/java/recap/P07_GET_OpenWeather.java
@@ -0,0 +1,15 @@
+package recap;
+
+public class P07_GET_OpenWeather {
+
+    /*
+       Endpoint: https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=YOUR_API_KEY
+       HTTP Metodu: GET
+       Senaryo:
+       Bu endpoint'e bir GET request gönderdiğimizde dönen Response’un:
+       Status code’unun 200,
+       Content-Type’ının application/json; charset=utf-8,
+       Response Body’sinde "name" alanının "Istanbul" olduğunu,
+       Response Body’sinde "main.temp" değerinin -50 ile 50 arasında olduğunu test ediniz.
+    */
+}
